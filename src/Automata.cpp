a4e701640818b08746dc9fe74e74e615cd19f53d
BIGShuu <renttrich@gmail.com>
2026-05-11 00:30:19 +0300

    Update Automata.cpp

--- src/Automata.cpp
+++ src/Automata.cpp
@@ -1,122 +1,124 @@
+// Copyright 2026 UNN-IASR
 #include "Automata.h"
 #include <sstream>
+#include <string>
 
 Automata::Automata() {
-    cash = 0;
-    state = OFF;
-    selectedDrink = -1;
-    
-    menuSize = 4;
-    menu = new std::string[menuSize];
-    prices = new int[menuSize];
-    
-    menu[0] = "Coffee";
-    menu[1] = "Tea";
-    menu[2] = "Hot Chocolate";
-    menu[3] = "Cappuccino";
-    
-    prices[0] = 50;
-    prices[1] = 30;
-    prices[2] = 60;
-    prices[3] = 70;
+  cash = 0;
+  state = OFF;
+  selectedDrink = -1;
+
+  menuSize = 4;
+  menu = new std::string[menuSize];
+  prices = new int[menuSize];
+
+  menu[0] = "Coffee";
+  menu[1] = "Tea";
+  menu[2] = "Hot Chocolate";
+  menu[3] = "Cappuccino";
+
+  prices[0] = 50;
+  prices[1] = 30;
+  prices[2] = 60;
+  prices[3] = 70;
 }
 
 Automata::~Automata() {
-    delete[] menu;
-    delete[] prices;
+  delete[] menu;
+  delete[] prices;
 }
 
 void Automata::on() {
-    if (state == OFF) {
-        state = WAIT;
-    }
+  if (state == OFF) {
+    state = WAIT;
+  }
 }
 
 void Automata::off() {
-    if (state == WAIT) {
-        state = OFF;
-        cash = 0;
-        selectedDrink = -1;
-    }
+  if (state == WAIT) {
+    state = OFF;
+    cash = 0;
+    selectedDrink = -1;
+  }
 }
 
 void Automata::coin(int amount) {
-    if (state == WAIT || state == ACCEPT) {
-        cash += amount;
-        state = ACCEPT;
-    }
+  if (state == WAIT || state == ACCEPT) {
+    cash += amount;
+    state = ACCEPT;
+  }
 }
 
 std::string Automata::getMenu() {
-    std::stringstream ss;
-    for (int i = 0; i < menuSize; i++) {
-        ss << i + 1 << ". " << menu[i] << " - " << prices[i] << " rub\n";
-    }
-    return ss.str();
+  std::stringstream ss;
+  for (int i = 0; i < menuSize; i++) {
+    ss << i + 1 << ". " << menu[i] << " - " << prices[i] << " rub\n";
+  }
+  return ss.str();
 }
 
 std::string Automata::getState() {
-    switch (state) {
-        case OFF: return "OFF";
-        case WAIT: return "WAIT";
-        case ACCEPT: return "ACCEPT";
-        case CHECK: return "CHECK";
-        case COOK: return "COOK";
-        default: return "UNKNOWN";
-    }
+  switch (state) {
+    case OFF: return "OFF";
+    case WAIT: return "WAIT";
+    case ACCEPT: return "ACCEPT";
+    case CHECK: return "CHECK";
+    case COOK: return "COOK";
+    default: return "UNKNOWN";
+  }
 }
 
 bool Automata::choice(int drinkIndex) {
-    if (state == ACCEPT && drinkIndex >= 0 && drinkIndex < menuSize) {
-        selectedDrink = drinkIndex;
-        state = CHECK;
-        return true;
-    }
-    return false;
+  if (state == ACCEPT && drinkIndex >= 0 && drinkIndex < menuSize) {
+    selectedDrink = drinkIndex;
+    state = CHECK;
+    return true;
+  }
+  return false;
 }
 
 bool Automata::check() {
-    if (state == CHECK && selectedDrink >= 0) {
-        if (cash >= prices[selectedDrink]) {
-            return true;
-        }
+  if (state == CHECK && selectedDrink >= 0) {
+    if (cash >= prices[selectedDrink]) {
+      return true;
     }
-    return false;
+  }
+  return false;
 }
 
 void Automata::cancel() {
-    if (state == ACCEPT || state == CHECK || state == COOK) {
-        cash = 0;
-        selectedDrink = -1;
-        state = WAIT;
-    }
+  if (state == ACCEPT || state == CHECK || state == COOK) {
+    cash = 0;
+    selectedDrink = -1;
+    state = WAIT;
+  }
 }
 
 void Automata::cook() {
-    if (state == CHECK && check()) {
-        cash -= prices[selectedDrink];
-        state = COOK;
-    }
+  if (state == CHECK && check()) {
+    cash -= prices[selectedDrink];
+    state = COOK;
+  }
 }
 
 void Automata::finish() {
-    if (state == COOK) {
-        state = WAIT;
-        selectedDrink = -1;
-    }
+  if (state == COOK) {
+    state = WAIT;
+    selectedDrink = -1;
+  }
 }
 
 STATES Automata::getCurrentState() const {
-    return state;
+  return state;
 }
 
 int Automata::getCash() const {
-    return cash;
+  return cash;
 }
 
 int Automata::getPrice(int index) const {
-    if (index >= 0 && index < menuSize) {
-        return prices[index];
-    }
-    return -1;
+  if (index >= 0 && index < menuSize) {
+    return prices[index];
+  }
+  return -1;
 }
