4d940a30d96f6b5ba994e0bd2107598a39fb0c56
BIGShuu <renttrich@gmail.com>
2026-05-11 00:26:16 +0300

    Update Automata.h

--- include/Automata.h
+++ include/Automata.h
@@ -1,44 +1,45 @@
-#ifndef AUTOMATA_H
-#define AUTOMATA_H
+// Copyright 2026 UNN-IASR
+#ifndef INCLUDE_AUTOMATA_H_
+#define INCLUDE_AUTOMATA_H_
 
 #include <string>
 
 enum STATES {
-    OFF,      // Выключено
-    WAIT,     // Ожидание
-    ACCEPT,   // Приём денег
-    CHECK,    // Проверка наличности
-    COOK      // Приготовление
+  OFF,
+  WAIT,
+  ACCEPT,
+  CHECK,
+  COOK
 };
 
 class Automata {
-private:
-    int cash;
-    std::string* menu;
-    int* prices;
-    int menuSize;
-    STATES state;
-    int selectedDrink;
+ private:
+  int cash;
+  std::string* menu;
+  int* prices;
+  int menuSize;
+  STATES state;
+  int selectedDrink;
 
-public:
-    Automata();
-    ~Automata();
-    
-    void on();
-    void off();
-    void coin(int amount);
-    std::string getMenu();
-    std::string getState();
-    bool choice(int drinkIndex);
-    bool check();
-    void cancel();
-    void cook();
-    void finish();
-    
-    // Дополнительные методы для тестирования
-    STATES getCurrentState() const;
-    int getCash() const;
-    int getPrice(int index) const;
+ public:
+  Automata();
+  ~Automata();
+
+  void on();
+  void off();
+  void coin(int amount);
+  std::string getMenu();
+  std::string getState();
+  bool choice(int drinkIndex);
+  bool check();
+  void cancel();
+  void cook();
+  void finish();
+
+  // Дополнительные методы для тестирования
+  STATES getCurrentState() const;
+  int getCash() const;
+  int getPrice(int index) const;
 };
 
-#endif // AUTOMATA_H
+#endif  // INCLUDE_AUTOMATA_H_
